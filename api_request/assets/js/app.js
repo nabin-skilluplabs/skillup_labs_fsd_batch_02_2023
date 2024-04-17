2e3fea635b67924809a2a8fdc248eb2c47981b90
Nabin Singh <nabin@skilluplabs.com.au>
2024-04-17 20:09:34 +1000

    Added additional features

--- api_request/assets/js/app.js
+++ api_request/assets/js/app.js
@@ -1,5 +1,5 @@
 (async () => {
-    const apiUrl = "https://v2.jokeapi.dev/joke/Programming,Misc?amount=2";
+    const apiUrl = "https://v2.jokeapi.dev/joke/Programming,Misc";
     const response = await fetch(apiUrl);
     const jokeData = await response.json();
     console.log(jokeData);
@@ -11,5 +11,38 @@
     else {
         console.log(`Joke: ${jokeData.setup} \n ${jokeData.delivery}`)
     }
-    
+    // Create a number input which takes number of jokes to grab,
+    // Create LOAD JOKES button, when clicked should load jokes using jokes api
+    // Populate those jokes in UI
 })();
+
+
+function populateJoke(joke) {
+   
+    let jokeString = '';
+    if(joke.type === "single") {
+        jokeString = joke.joke;
+    }
+    else {
+        jokeString = `${joke.setup} <br> ${joke.delivery}`;
+    }
+    document.getElementById("joke_list").insertAdjacentHTML( 'beforeend', `<h2>${jokeString}</h2>` ) 
+}
+
+async function  loadJokes  () {
+    const amount = document.getElementById("amount").value;
+    const apiUrl = `https://v2.jokeapi.dev/joke/Programming,Misc?amount=${amount}`;
+    const response = await fetch(apiUrl, {
+        method: 'GET'
+    });
+    const jokeData = await response.json();
+    document.getElementById("joke_list").innerHTML = "";
+    if(jokeData.jokes) {
+        jokeData.jokes.forEach(joke => {
+            populateJoke(joke);
+        });
+    }
+    else {
+        populateJoke(jokeData);
+    }
+}
