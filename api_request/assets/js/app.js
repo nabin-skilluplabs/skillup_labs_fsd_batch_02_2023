5d922207bd07e36b3dd89899434bde25aa104c1b
Saurav Thapa <155595327+Saurav755@users.noreply.github.com>
2024-04-17 20:09:55 +1000

    added some features

---
+++ api_request/assets/js/app.js
@@ -0,0 +1,42 @@
+(async () => {
+    const apiUrl = `https://v2.jokeapi.dev/joke/Programming,Misc?amount=${document.getElementById('amount')}`;
+    document.getElementById('amount')
+    const repsonse = await fetch(apiUrl);
+    const jokesData = await respose.json();
+    console.log(jokeData);
+    console.log(`Category: ${jokeData.category}`);
+    
+    if(jokeData.type === "single"){
+        console.log(`joke: ${jokeData.joke}`);
+
+    }else {
+        console.log(`joke: $(jokeData.setup) /n ${jokeData.delivery}`);
+    
+    }
+
+    
+})();
+
+function poplateJoke(joke){
+    let jokeString = "";
+    if (joke.type === "single"){
+        jokeString = joke.joke;
+
+    }else {
+        jokeString = `${joke.setup} /n ${joke.delivery}`;
+    document.getElementById("joke_list").innerHTML = `<h2>${joke.joke}</h2>`;
+}
+
+async function loadJokes (){
+    const amount = document.getElementById('amount').ariaValueMax;
+    console.log({amount});
+    const apiUrl = `https://v2.jokeapi.dev/joke/Programming,Misc?amount=${amount}`;
+    const repsonse = await fetch(apiUrl);
+    const jokesData = await respose.json();
+    if(jokesData.type){
+        jokeData.jokes.forEach(element => {
+            populateJoke(joke);
+        });
+    }else{
+        poplateJoke(jokeData);
+    }
