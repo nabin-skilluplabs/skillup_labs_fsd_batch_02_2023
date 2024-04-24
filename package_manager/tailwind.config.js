6494fb7671517e49b8043262805c655f56fd70b2
Saurav Thapa <155595327+Saurav755@users.noreply.github.com>
2024-04-24 20:34:54 +1000

    added tailwind npm

---
+++ package_manager/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.js", "./src/**/*.html"],
+  content: ["./src/**/*.${html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
