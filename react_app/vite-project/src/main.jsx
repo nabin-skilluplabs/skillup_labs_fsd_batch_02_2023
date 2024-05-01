519594d646406ceed55555e8241d73afec60bc65
Nabin Singh <nabin@skilluplabs.com.au>
2024-05-01 19:41:57 +1000

    Added router

--- react_app/vite-project/src/main.jsx
+++ react_app/vite-project/src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
+import JobReadyProgram from './pages/JobReadyProgram.jsx';
+import About from './pages/About.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/job-ready-program",
+    element: <JobReadyProgram />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ react_app/vite-project/src/pages/JobReadyProgram.jsx
@@ -0,0 +1,9 @@
+function JobReadyProgram() {
+    return(
+        <div>
+            <h1>Job Ready Program</h1>
+        </div>
+    );
+}
+
+export default JobReadyProgram;
